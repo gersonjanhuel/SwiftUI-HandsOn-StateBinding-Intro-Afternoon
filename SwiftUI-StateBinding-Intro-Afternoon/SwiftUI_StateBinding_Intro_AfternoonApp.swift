b92a394c59f8bf5cd5083d09b227ba6517a690e5
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-04-06 15:26:00 +0700

    Initial Commit

---
+++ SwiftUI-StateBinding-Intro-Afternoon/SwiftUI_StateBinding_Intro_AfternoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StateBinding_Intro_AfternoonApp.swift
+//  SwiftUI-StateBinding-Intro-Afternoon
+//
+//  Created by Gerson Janhuel on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StateBinding_Intro_AfternoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
